a812bf0313d70a34dcd3a0d3b403cb3d45f24c0c
Kiwon Seok <seok.kiwon@gmail.com>
2017-11-10 09:42:23 +0900

    added jsx test (#12)

--- sbt-webpack-tester/src/main/web/js/entry.js
+++ sbt-webpack-tester/src/main/web/js/entry.js
@@ -1,1 +1,5 @@
-document.write(require("./content.js"));
+import { content } from './content'
+import { Hello } from './hello'
+
+document.write(content)
+console.log(Hello)
---
+++ src/sbt-test/sbt-webpack/jsx/src/main/web/js/entry.js
@@ -0,0 +1,3 @@
+import { Hello } from './hello'
+
+console.log(Hello)
